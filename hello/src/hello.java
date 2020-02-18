3c63a29e65ff9493e4bc7bbdb79622a370f3f301
lly <744723577@qq.com>
2020-02-18 11:34:24 +0800

    修改

--- hello/src/hello.java
+++ hello/src/hello.java
@@ -2,6 +2,10 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("bb");
+		System.out.println("bb");
+		System.out.println("bb");
+		System.out.println("bb");
+		System.out.println("bb");
     }
 
 }
